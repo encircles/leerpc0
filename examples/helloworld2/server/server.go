1ad67a619b3192e6bd3391f99165c452643636b5
encircles <encircles@163.com>
2020-07-24 11:42:09 +0800

    del log

--- examples/helloworld2/server/server.go
+++ examples/helloworld2/server/server.go
@@ -1,1 +1,19 @@
-package server
+package main
+
+import (
+	"time"
+
+	"github.com/encircles/leerpc0"
+)
+
+func main() {
+	opts := []leerpc0.ServerOption{
+		leerpc0.WithAddress("127.0.0.1:8000"),
+		leerpc0.WithNetwork("tcp"),
+		leerpc0.WithProtocol("proto"),
+		leerpc0.WithTimeout(time.Millisecond * 2000),
+	}
+
+	_ = leerpc0.NewServer(opts...)
+
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.4.2
+	github.com/lubanproj/protoc-gen-gorpc v0.0.0-20200602052016-6d2838072e88 // indirect
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/stretchr/testify v1.4.0
 	github.com/vmihailenco/msgpack v4.0.4+incompatible
